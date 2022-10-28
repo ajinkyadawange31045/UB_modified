08367fbc4c486aec328416816c4fb78fde67605b
Ajinkya Dawange <ajinkyadawange333ard@gmail.com>
2022-10-28 19:53:27 +0530

    signin and login

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home, post,category
+from .views import home, post,category,user_signup, user_login, user_logout
 
 
 urlpatterns = [
     path('', home),
     path('blog/<slug:url>', post),
     path('category/<slug:url>',category),
+    path('signup/',user_signup,name='signup'),
+    path('login/',user_login,name='login'),
+    path('logout/',user_logout,name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.contrib.auth.forms import UserCreationForm
+# from .forms import SignUpForm, LoginForm, PostForm
+from .forms import SignUpForm, LoginForm
+from django.contrib import messages
+from django.contrib.auth import  authenticate,login,logout
 from blog.models import Post, Category, Team, Value
+from django import forms
+from django.contrib.auth.models import Group
 
 
 # Create your views here.
@@ -29,3 +36,44 @@ def category(request, url):
     cat = Category.objects.get(url=url)
     posts = Post.objects.filter(cat=cat)
     return render(request, "category.html", {'cat': cat, 'posts': posts})
+
+
+
+#logout
+def user_logout(request):
+    logout(request)
+    return HttpResponsePermanentRedirect('/')
+
+#login
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request =request,data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(username=uname,password = upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged in Successfully')
+                    return HttpResponsePermanentRedirect('/')
+        else:
+            form = LoginForm()
+        return render(request,'login.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/')
+
+
+#signup
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Signed in successfully')
+            user = form.save()
+            # group = Group.objects.get(name ='Author')
+            # user.groups.add(group)
+    else:
+        form = SignUpForm()
+    return render(request,'signup.html',{'form':form})
+
